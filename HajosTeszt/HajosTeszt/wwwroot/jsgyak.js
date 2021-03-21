a54fb8c7a4e31c7944b9b9138b4952c2eabb25d2
Vazul Tasi <daniel.tasi@stud.uni-corvinus.hu>
2021-03-21 23:37:58 +0100

    jsgyak

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,14 @@
+﻿window.onload = () => {
+    let hova = document.getElementById("pascal");
+    for (var s = 0; s < length; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("sor");
+        hova.appendChild("sor");
+        for (var o = 0; o < length; o++) {
+            let szám = document.createElement("div");
+            szám.innerText = (s + 1) * (o + 1);
+            sor.appendChild("szám");
+            szám.classList.add("elem");
+        }
+    }
+}
