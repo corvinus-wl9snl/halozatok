962fe927b11b581f9c6fdbbc6d9d30bb334c64a5
Vazul Tasi <daniel.tasi@stud.uni-corvinus.hu>
2021-03-28 22:36:03 +0200

    hetedik

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,21 @@
+﻿var kérdések;
+function letöltés() {
+    fetch('/questions.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(k) {
+    console.log("Sikeres letöltés")
+    console.log(k)
+    kérdések = k;
+}
+
+window.onload(letöltés());
+
+function kérdésMegjelenítés(kérdés) {
+    document.getElementById("vissza").onclick = function () {
+
+    }
+}
