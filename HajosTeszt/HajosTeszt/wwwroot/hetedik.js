01f509a6eecda916708ce27cce34a6d6f19a6e6f
Vazul Tasi <daniel.tasi@stud.uni-corvinus.hu>
2021-04-25 15:25:23 +0200

    hajos9

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,4 +1,5 @@
 ﻿var kérdések;
+
 function letöltés() {
     fetch('/questions.json')
         .then(response => response.json())
@@ -6,16 +7,50 @@ function letöltés() {
        );
 }
 
+window.onload(letöltés());
 function letöltésBefejeződött(k) {
     console.log("Sikeres letöltés")
     console.log(k)
     kérdések = k;
 }
 
-window.onload(letöltés());
+
+fetch('/questions/4')
+    .then(response => response.json())
+    .then(data => console.log(data)
+);
+
+fetch('/questions/1')
+    .then(response => response.json())
+    .then(data => kérdésMegjelenítés(data)
+);
 
 function kérdésMegjelenítés(kérdés) {
-    document.getElementById("vissza").onclick = function () {
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+}
 
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}
+function válaszfeldolgozás(válasz) {
+    if (!válasz.ok) {
+        console.error(`Hibás válasz: ${response.status}`)
+    }
+    else {
+        return válasz.json()
     }
 }
